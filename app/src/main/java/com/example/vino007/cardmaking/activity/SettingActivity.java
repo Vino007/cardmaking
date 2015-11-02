99f8ac9bcda9a3b8823685d1f114d1f9a01d994b
vino007 <540134090@qq.com>
2015-11-02 16:42:34 +0800

    IC制卡app初步

---
+++ app/src/main/java/com/example/vino007/cardmaking/activity/SettingActivity.java
@@ -0,0 +1,16 @@
+package com.example.vino007.cardmaking.activity;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import com.example.vino007.cardmaking.R;
+
+public class SettingActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+    }
+
+}
